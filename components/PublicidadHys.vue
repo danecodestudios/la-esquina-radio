ef3a2ae938752d421b96568064aa553f7e292a67
unknown <danecodsas@gmail.com>
2021-10-23 11:23:56 -0500

    Actualizacion

---
+++ components/PublicidadHys.vue
@@ -0,0 +1,60 @@
+<template>
+<v-carousel :height="height" cycle :interval="intervalo" :show-arrows="true" hide-delimiters  >
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      class="w-100"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    >
+    
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<style scoped>
+
+
+
+</style>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        intervalo: 6000,
+        items: [
+          {
+            src: '/hys/01.png',
+          },
+          {
+            src: '/hys/02.png',
+          },
+          {
+            src: '/hys/03.png',
+          },
+          {
+            src: '/hys/04.png',
+          },
+            {
+            src: '/hys/05.png',
+          },
+        ],
+      }
+    },
+
+    computed:{
+        height () {
+        switch (this.$vuetify.breakpoint.name) {
+          case 'xs': return 50
+          case 'sm': return 80
+          case 'md': return 100
+          case 'lg': return 135
+          case 'xl': return 200
+        }
+    }
+  }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,5 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline text-center">
-          <marquee behavior="" direction="">BIENVENIDOS A LA ESQUINA RADIO  </marquee> 
-        </v-card-title>
-        <v-card-text>
-          <p class="headline  text-center">PAGINA EN CONTRUCCION.</p>
-          <p class="text-center">
-           <a
-            class="ml-2"
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <v-icon class="" >mdi-facebook</v-icon>
-            </a>
-
-               <a
-            class="ml-2 "
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <v-icon class=" red--text accent-4  " >mdi-youtube</v-icon>
-            </a>
-          </p>
-         
-        </v-card-text>
-
-      </v-card>
-    </v-col>
-  </v-row>
+<Publicidad-hys />
 </template>
+
+
