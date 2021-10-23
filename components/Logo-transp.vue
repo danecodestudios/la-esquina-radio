737b8ec082a05c147970a7f2ebd22c09058749cf
unknown <danecodsas@gmail.com>
2021-10-23 08:24:13 -0500

    first commit

---
+++ components/Logo-transp.vue
@@ -0,0 +1,22 @@
+<template>
+  <img
+    class="LaEsquinaLogo"
+    alt="LA ESQUINA RADIO"
+    src="/logo-trasp.svg"
+  >
+</template>
+
+<style>
+.LaEsquinaLogo {
+  height: 150px;
+  width: 150px;
+  transform: rotateY(560deg);
+  animation: turn 3.5s ease-out forwards 1s;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateY(0deg);
+  }
+}
+</style>
---
+++ pages/noticias.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;BIENVENIDOS A NUESTRA PAGINA DE NOTICIAS LA ESQUINA RADIO &#8221;
+        <footer>
+          <small>
+            <em>&mdash;DESARROLLO POR DANILO BUSTILLO</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
